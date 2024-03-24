23cf5d2de27cad4c5b6a0f09c652a905f00269eb
arthur <baitichhamza@gmail.com>
2024-03-24 23:23:50 +0100

    test git

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,4 +1,4 @@
-<x-card class="!p-10  ">
+<x-card class="!p-12  ">
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block" src=" {{asset('images/no-image.png')}} " alt="" />
         <div>
