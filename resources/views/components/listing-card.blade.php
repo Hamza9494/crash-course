4e6e5175c38a4fae7c7d66ed7614114b9cd6b27e
arthur <baitichhamza@gmail.com>
2024-03-25 22:02:50 +0100

    create seperate tags component

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,3 +1,4 @@
+@props(['listing'])
 <x-card class="!p-12  ">
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block" src=" {{asset('images/no-image.png')}} " alt="" />
@@ -6,20 +7,7 @@
                 <a href="listing/{{$listing->id}} "> {{$listing->title}} </a>
             </h3>
             <div class="text-xl font-bold mb-4"> {{$listing->comapny}} </div>
-            <ul class="flex">
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Laravel</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">API</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Backend</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <x-listing-tags :tagsCsv="$listing->tags" />
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
             </div>
