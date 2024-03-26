25432625fe7d6b67f15972511dbb20c2d0aad6bb
arthur <baitichhamza@gmail.com>
2024-03-26 23:03:38 +0100

    fix session message bug

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,6 @@
 @if (session()->has('success')) {
-<div class="fixed top-0 transform bg-laravel left-1/2 transform-translate-x1/2 text-white px-48 py-3"></div>
+<div class="fixed top-0 transform bg-laravel left-1/2 transform-translate-x1/2 text-white px-48 py-3">
+    {{session('success')}} </div>
 }
 
 @endif
